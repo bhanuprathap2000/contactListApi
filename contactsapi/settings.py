40af3cc012a35869de59f304f0055c1485b60186
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2021-12-29 19:04:58 +0530

    Update settings.py

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -99,8 +99,10 @@ WSGI_APPLICATION = 'contactsapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'contactlistdb',
+        'USER': 'root',
+        'PASSWORD': 'bhanuprathap',
     }
 }
 
